e991757c42ce3f522ca1286af052ddf983114c89
tal1974 <tal1974@yahoo.com>
2024-07-29 16:13:33 +0300

    small change

--- replenish/idena_replenish.cpp
+++ replenish/idena_replenish.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class utl {
  public:
   utl(string url, string key) : m_url(url), m_key(key) {}
-  const json post(json);
+  const json post(json&);
 
  private:
   const cpr::Url m_url;
@@ -18,17 +18,17 @@ int main(int argc, char* argv[]) {
     cout << "Usage: idena_replenish <address:port> <api.key> <min start "
             "balance> <balance remain after replenish>"
          << endl;
-    cout << "Example: idena_replenish http://127.0.0.1:9119 123apikey 10 2";
+    cout << "Example: idena_replenish http://127.0.0.1:9119 123apikey 10 2"
+         << endl;
     return 0;
   }
   utl l_utl(argv[1], argv[2]);
 
-  string l_addr = l_utl.post(
-      R"({"method":"dna_getCoinbaseAddr","params":[],"id":1,"key":""})"_json)
-                      ["result"];
-  cout << "Address is " << l_addr << endl;
   auto ls_body =
-      R"({"method":"dna_getBalance","params":[],"id":2,"key":""})"_json;
+      R"({"method":"dna_getCoinbaseAddr","params":[],"id":1,"key":""})"_json;
+  string l_addr = l_utl.post(ls_body)["result"];
+  cout << "Address is " << l_addr << endl;
+  ls_body = R"({"method":"dna_getBalance","params":[],"id":2,"key":""})"_json;
   ls_body["params"][0] = l_addr;
   double replenish_sum = stod(string(l_utl.post(ls_body)["result"]["balance"]));
   cout << "Balance is " << replenish_sum << endl;
@@ -45,17 +45,17 @@ int main(int argc, char* argv[]) {
   cout << "Transaction " << l_utl.post(ls_body);
   return 0;
 }
-const json utl::post(json i_json) {
+const json utl::post(json& i_json) {
   i_json["key"] = m_key;
   auto lPost = cpr::Post(m_url, cpr::Body{i_json.dump()},
                          cpr::Header{{"Content-Type", "application/json"}});
   if (lPost.status_code != 200) {
-    cout << lPost.error.message;
+    cout << lPost.error.message << endl;
     exit(-1);
   }
   auto jRes = json::parse(lPost.text);
   if (jRes.find("error") != jRes.end()) {
-    cout << jRes["error"]["message"];
+    cout << jRes["error"]["message"] << endl;
     exit(-1);
   }
   return jRes;
